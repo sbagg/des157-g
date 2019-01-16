6df8f698f1dc7cd64e3a6a0afce0632286d0558f
Sabrina Baggett <sabaggett@ucdavis.edu>
2019-01-16 00:09:29 -0800

    background updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('reading js');
 
-var tParent = 255, cSize = 0;
+//var tParent = 255, cSize = 0;
 
 /*ar cCircle = {
   x:mouseX;
@@ -8,18 +8,21 @@ var tParent = 255, cSize = 0;
   noFill();
 }*/
 
-function setup(){
+/*function setup() {
 
   var myCanvas = createCanvas(800,250);
   myCanvas.parent('mySketch');
   noFill();
 
-}
+}*/
 //connect myCanvas to mySketch
-function draw(){
-
-
-  if(cSize < 350){
+//function draw() {
+    var string = "Welcome to My Portal.";
+    var typeString = string.split("");
+    var mydiv = document.getElementById('mySketch');
+    mydiv.addEventListener("mouseover", animate);
+    mydiv.addEventListener("mouseout", stopAnimate);
+  /*if(cSize < 350){
     tParent--;
     ellipse(mouseX, mouseY, cSize, cSize);
     noFill();
@@ -30,6 +33,13 @@ function draw(){
     noFill();
 
 
-  }
+  }*/
 
+//}
+function animate() {
+    typeString.length > 0 ? mydiv.innerHTML += typeString.shift() : clearTimeout(running); 
+    var running = setTimeout(animate, 90);
+}
+function stopAnimate() {
+    mydiv.innerHTML = "";
 }
