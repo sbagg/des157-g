15568937a8a66bb7b7e36f425fb9d4cf341739cc
Sabrina Baggett <sabaggett@ucdavis.edu>
2019-01-30 01:58:21 -0800

    MadLib 2.0

--- script.js
+++ script.js
@@ -1,41 +1,11 @@
 console.log('reading js');
 
-//var tParent = 255, cSize = 0;
 
-/*ar cCircle = {
-  x:mouseX;
-  y:mouseY;
-  noFill();
-}*/
-
-/*function setup() {
-
-  var myCanvas = createCanvas(800,250);
-  myCanvas.parent('mySketch');
-  noFill();
-
-}*/
-//connect myCanvas to mySketch
-//function draw() {
     var string = "//Welcome to My Portal;";
     var typeString = string.split("");
     var mydiv = document.getElementById('mySketch');
     mydiv.addEventListener("mouseover", animate);
-    //mydiv.addEventListener("click", changeColor);
-  /*if(cSize < 350){
-    tParent--;
-    ellipse(mouseX, mouseY, cSize, cSize);
-    noFill();
-    cSize++;
-  }else{
-    tParent--;
-    ellipse(mouseX, mouseY, cSize, cSize);
-    noFill();
 
-
-  }*/
-
-//}
 function animate() {
     typeString.length > 0 ? mydiv.innerHTML += typeString.shift() : clearTimeout(running); 
     var running = setTimeout(animate, 90);
--- studio1/script.js
+++ studio1/script.js
@@ -1,41 +1,34 @@
-"use strict";
+'use strict';
+console.log('reading js');
 
-/*capture the submit event with an event listener for onclick and a callback function of processForm
-*/
-document.querySelector("#submit").addEventListener('click', processForm);
-document.querySelector("#reset").addEventListener('click', resetForm);
-
-//define process function
-function processForm(evt) {
-  // capture html form inputs
+document.querySelector('#s').addEventListener('click', processForm);
+var el = document.querySelector('#back');
+if(el){
+   el.addEventListener('click', resetForm);
+}
+function processForm (evt){
   var proNoun = document.querySelector("#proNoun").value;
   var adj1 = document.querySelector("#adj1").value;
   var verb1 = document.querySelector("#verb1").value;
   var adj2 = document.querySelector("#adj1").value;
   var verb2 = document.querySelector("#verb1").value;
   var adv1 = document.querySelector("#adv1").value;
-
+  var x = document.getElementById("myImg").src;
   // caputre myMsg
-  var myMsg = document.querySelector("#myMsg");
-  var msgSection = document.querySelector("#msgSection");
-
-
-  myMsg.innerHTML =
-    "Hi, " +
-    userName +
-    "!<p>You love the color <em>" +
-    favColor +
-    "</em>!  Have a great day!</p>";
-
-  msgSection.className = "grow";
-
-  // prevent page from reloading
+  var madLib = document.querySelector("#madLib"); //message
+  var madLibMessage = document.querySelector("#madLibsMessage");
+  var msg = document.querySelector("#msg");//section
+  var article = document.querySelector("article");
+  var section = document.querySelector("section");
+    
+  article.style.display = "none";
+  section.style.display = "block";
+  msg.style.display = "block";
+  madLibMessage.innerHTML = 'Jamie absolutely love to type, it is ' +proNoun + ' favorite thing to do. The first step to a good typing method is to have a very ' + adj1 + ' keyboard.  It is always good to ' + verb1 + ' with the keyboard to get better at using it.  Then, you must make sure your computer is very ' + adj2 + ' so that you can ' + verb2 + ' it properly. With these steps, you too can type ' + adv1 + '!'; 
+  
   evt.preventDefault();
 }
 
-function submitForm() {
-  msgSection.className = "grow";
-}
-function returnForm() {
-  msgSection.className = "shrink";
+function back(){
+  location.reload(true);
 }
