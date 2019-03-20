691f5267d6cf17326df1c02440ae270312c4b7fa
Sabrina Baggett <sabaggett@ucdavis.edu>
2019-03-20 14:44:56 -0700

    Final- still working on Firebase!

--- final/create/drama/script.js
+++ final/create/drama/script.js
@@ -1,5 +1,8 @@
 'use strict';
 console.log('reading js');
+function reload(){
+  location.reload(true);
+}
 
 var create = document.querySelector("article");
 var formData = document.querySelector("form");
@@ -12,7 +15,20 @@ var add = document.querySelector("#add");
 var welcome = document.querySelector('h1');
 var notes = document.querySelector(".notes");
 var deleteButton = document.querySelector("#delete");
+  var teaser = document.querySelector("#teaser").value;
+  var act1 = document.querySelector("#act1").value;
+  var act2 = document.querySelector("#act2").value;
+  var act3 = document.querySelector("#act3").value;
+  var act4 = document.querySelector("#act4").value;
+  var act5 = document.querySelector("#act5").value;
+  // caputre myMsg
+  var scriptSection = document.querySelector("#scriptSection");//section
+  var scriptMsg = document.querySelector("#scriptMsg");
 
+deleteButton.addEventListener('click', reload);
+back.addEventListener('click', function(e){
+    document.location.href = "../../index.html";
+});
 create.addEventListener('click', function(e){
     //create.style.display = "none";
    
@@ -28,9 +44,20 @@ create.addEventListener('click', function(e){
     back.style.marginTop= "7%";
     welcome.style.display = "none";
     add.style.display = "block";
-    finish.style.display = "block";
+    finish.style.display = "none";
     deleteButton.style.display = "block";
     
     e.preventDefault();
 
 });
+
+add.addEventListener('click', function(e){
+    document.location.href = "../../index.html";
+});
+
+scriptMsg.innerHTML = teaser + '<br /><br />'
+                      'ACT 1<br /><br />' + act1 +
+                      'ACT 2<br /><br />' + act2 +
+                      'ACT 3<br /><br />' + act3 +
+                      'ACT 4<br /><br />' + act4 +
+                      'ACT 5<br /><br />' + act5 ; 
